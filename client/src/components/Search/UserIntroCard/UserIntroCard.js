6f272155eb7d4fd7107d7f7d64ac6b332a855f98
Cefalu Wave <luke.evan.graham@gmail.com>
2022-03-31 23:50:57 -0700

    prettier search results

--- client/src/components/Search/UserIntroCard/UserIntroCard.js
+++ client/src/components/Search/UserIntroCard/UserIntroCard.js
@@ -1,5 +1,6 @@
 import React from "react";
-import InitialProfileImage from "../../UI/InitalProfileImage/InitialProfileImage"
+import InitialProfileImage from "../../UI/InitalProfileImage/InitialProfileImage";
+import Button from "../../UI/Button/Button";
 
 import classes from "./UserIntroCard.module.scss";
 
@@ -10,11 +11,19 @@ const UserIntroCard = ({ user }) => (
       <img
         src={user.ProfilePicture.image}
         alt={user.ProfilePicture.title}
-        srcSet={`${user.ProfilePicture.image.replace("upload/", "upload/w_125/")} 125w`}
+        srcSet={`${user.ProfilePicture.image.replace(
+          "upload/",
+          "upload/w_125/"
+        )} 125w`}
       />
-    ) : <InitialProfileImage user={user} size={100} /> }
+    ) : (
+      <InitialProfileImage user={user} size={80} />
+    )}
     <div className={classes.name}>
       {user.firstName} {user.lastName}
+    </div>
+    <div className={classes.followButton}>
+      <Button color={"green"}>Follow</Button>
     </div>
   </div>
 );
